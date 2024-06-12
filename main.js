ca88a53a2f1fafc464cbefff09d76c2264c43841
tatianaD <angietatiana167@gmail.com>
2024-06-12 21:25:32 +0200

    It is verified that the number of empanadas is not exceeded

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ export const fn = (a, b, c) => {
    const precioB = 14;     
    const precioC = 16;     
 }
+
+
+//verifico que ninguna de las cantidades de empanadas sean 0
+if (a < 0 || b < 0 || c < 0) {                                              
+   throw new Error("Introduzca una cantidad de empanadas valida");
+}
+
+// verif que no se exceda de 40 empanadas
+else if ((a + b + c) >= 40) {                                                 
+   throw new Error("Excede la cantidad máxima de empanadas");
+}
