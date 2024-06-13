86ddcd3423978be5e4a3a8576c61b90eda63f029
tatianaD <angietatiana167@gmail.com>
2024-06-13 17:46:24 +0200

    conditions to try 3x1

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 export const fn = (a, b, c) => {
 
-   const precioA = 12;     
-   const precioB = 14;     
-   const precioC = 16;     
-}
+  const precioA = 12;     
+  const precioB = 14;     
+  const precioC = 16;     
 
+  if (a < 0 || b < 0 || c < 0) {                                                
+     throw new Error("Introduzca una cantidad de empanadas válida");
+  }
 
-//verifico que ninguna de las cantidades de empanadas sean 0
-if (a < 0 || b < 0 || c < 0) {                                              
-   throw new Error("Introduzca una cantidad de empanadas valida");
-}
+  else if ((a + b + c) >= 40) {                                                
+     throw new Error("Excede la cantidad máxima de empanadas");
+  }
 
-// verif que no se exceda de 40 empanadas
-else if ((a + b + c) >= 40) {                                                 
-   throw new Error("Excede la cantidad máxima de empanadas");
+  else if ((a + b + c) % 3 != 0) {                                              
+     throw new Error("No se aplica oferta 3x1");
+  }
+  else {
+     let salida = Math.ceil(((a * precioA) + (b * precioB) + (c * precioC)) / 3);        
+
+     return salida;                                                                        
+  };
+
 }
